234effdbbd583fee1a568b4cbeb96c6bc560433e
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-27 15:07:16 +0200

    aggiunta struttura base esercizio

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*
+Esercizio Campo Minato:
+
+Consegna
+-Generare una griglia di gioco quadrata , in cui ogni cella contiene un numero incrementale tra quelli compresi tra 1 e 100
+-Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
+Passaggi da fare:
+-Creo la mia variabile 
+-Creo una funzione per generare i quadrati delle mie celle
+-Creo tramite una formula, i numeri in modo incrementale da 1 a 100
+-Tramite un addEvenentListner creo un evento sul click delle varie celle,che dovranno colorarsi al click del nostro utente
+
+Se riesco continuo anche con i bonus
+*/
